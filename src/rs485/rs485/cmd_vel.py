e0da4f1dca275bb4d8aabe5fa5d66a061039ca84
gongminsu-maker <gongminsu@naver.com>
2025-08-01 19:37:06 +0900

    test3

--- src/rs485/rs485/cmd_vel.py
+++ src/rs485/rs485/cmd_vel.py
@@ -12,8 +12,9 @@ class MotorControllerNode(Node):
         super().__init__('motorcontroller_node')
         # 키보드 속도 명령 수신
         self.sub = self.create_subscription(Twist,'cmd_vel',self.motor_control,10)
+
         # serial setting
-        self.ser = serial.Serial(
+        self.ser_R = serial.Serial(
             port='/dev/ttyUSB0',         # 적절한 포트로 수정 (ls /dev/ttyUSB*을 통해 포트번호 확인하기)
             baudrate=9600,
             bytesize=serial.EIGHTBITS,
@@ -21,20 +22,32 @@ class MotorControllerNode(Node):
             stopbits=serial.STOPBITS_ONE,
             timeout=1
         )
+        self.ser_L = serial.Serial(
+            port="/dev/ttyUSB1",
+            baudrate=9600,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=1 
+        )
 
         # 변수 선언
-        self.declare_parameter("receiver","speed_feedback")
-        self.receiver = self.get_parameter("receiver").get_parameter_value().string_value
-        receiver_func = getattr(self, self.receiver)
+        #self.declare_parameter("receiver","speed_feedback")
+        #self.receiver = self.get_parameter("receiver").get_parameter_value().string_value
+        #receiver_func = getattr(self, self.receiver)
+
         # 헤더 & ID
         self.header = [0xFF, 0xFE]
         self.motor_id = 0x00
         # 피드백 요청 timer
         self.timer = self.create_timer(1.0, self.request_feedback)
-        # 백그라운드 수신 쓰레드 시작
-        self.receiver_thread = threading.Thread(target=receiver_func, daemon=True)
-        self.receiver_thread.start()
 
+        # 백그라운드 수신 쓰레드 시작
+        self.receiver_thread_R = threading.Thread(target=self.speed_feedback_R, daemon=True)
+        self.receiver_thread_L = threading.Thread(target=self.speed_feedback_L, daemon=True)        
+        self.receiver_thread_R.start()
+        self.receiver_thread_L.start()
+        
     def motor_control(self,msg):
         if msg.linear.x > 0:
             self.direction = 0x01 #CW
@@ -43,7 +56,7 @@ class MotorControllerNode(Node):
         vel = abs(msg.linear.x)                        # [m/s]이며 cmd_vel을 통해 들어옴 max 0.26m/s
         radius = 0.135                            #[m]이며 실제 로봇 휠 반지름
         rpm = round((vel/radius)*(60/(2*m.pi)),1) # rpm변환 소수점 첫째자리 반올림 (0.1rpm단위이기 때문에)
-        speed_int = int(rpm*10)                   # 계산된 rpm을 실제 프로토콜에 맞는 rpm으로 변환 ex(1300rpm을 명령해도 130rpm으로 돈다)
+        speed_int = int(rpm*10)                   # 계산된 rpm을 실제 프로토콜에 맞는 rpm으로 변환 ex(1300rpm을 명령하면 130rpm으로 돈다)
         
         #속도는 프로토콜 상 2byte임, 1byte의 데이터 크기를 가짐으로 상위 byte, 하위 byte로 나눔
         MSB = (speed_int >> 8) & 0xFF # 상위 byte
@@ -57,29 +70,59 @@ class MotorControllerNode(Node):
         data_size = 0x06
         mode = 0x03   
         time_to_reach = 0x0A  # 14 = 2.0초 0A = 1.0초 
-        checksum = (~(self.motor_id + data_size + mode + self.direction + self.speed[0] + self.speed[1] + time_to_reach)) & 0xFF
-
-        data = [checksum]+ [mode, self.direction, self.speed[0],self.speed[1],time_to_reach]
-        full_packet = self.header + [self.motor_id] + [data_size] + data
+        #Right motor
+        checksum_R = (~(self.motor_id + data_size + mode + self.direction + self.speed[0] + self.speed[1] + time_to_reach)) & 0xFF
+        data_R = [checksum_R]+ [mode, self.direction, self.speed[0],self.speed[1],time_to_reach]
+        full_packet_R = [self.header, self.motor_id, data_size , data_R]
+        #Left motor
+        checksum_L = (~(self.motor_id + data_size + mode + abs(self.direction-1) + self.speed[0] + self.speed[1] + time_to_reach)) & 0xFF
+        data_L = [checksum_L] + [mode, abs(self.direction-1), self.speed[0], self.speed[1], time_to_reach]
+        full_packet_L = [self.header, self.motor_id , data_size, data_L]
 
         # 전송
-        self.ser.write(bytearray(full_packet))
-        self.get_logger().info(f"Sent packet: {[hex(b) for b in full_packet]}")
+        self.ser_R.write(bytearray(full_packet_R))
+        self.ser_L.write(bytearray(full_packet_L))
+        self.get_logger().info(f"Sent packet to Right: {[hex(b) for b in full_packet_R]}")
+        self.get_logger().info(f"Sent packet to Left: {[hex(b) for b in full_packet_L]}")
 
     def request_feedback(self):
         data_size = 0x02
-        mode = 0xA2       
-        checksum = (~(self.motor_id + data_size + mode)) & 0xFF
-        data = [checksum] + [mode]
-        full_packet = self.header + [self.motor_id, data_size] + data
+        mode = 0xA2
+        #Right motor
+        checksum_R = (~(self.motor_id + data_size + mode)) & 0xFF
+        data_R = [checksum_R , mode]
+        full_packet_R = self.header + [self.motor_id, data_size] + data_R
+
+        #Left motor
+        checksum_L = (~(self.motor_id + data_size + mode)) & 0xFF
+        data_L = [checksum_L , mode]
+        full_packet_L = self.header + [self.motor_id, data_size], data_L
 
         #전송
-        self.ser.write(bytearray(full_packet))
-        self.get_logger().info(f"[TX] {[ hex(b) for b in full_packet]}")
+        self.ser_R.write(bytearray(full_packet_R))
+        self.ser_L.write(bytearray(full_packet_L))
+        self.get_logger().info(f"[TX] {[ hex(b) for b in full_packet_R]}")
 
-    def speed_feedback(self):
+    def speed_feedback_R(self):
         while True:
-            response = self.ser.read(12)
+            response = self.ser_R.read(12)
+            if response and len(response) >= 12:
+                header = response[0:2]
+                data_size = response[3]
+                speed = ((response[7] << 8) | response[8]) * 0.1     # 0.1 [rpm]
+                position = ((response[9] << 8) | response[10]) * 0.1 # 0.1 [degree]
+                current = response[11] * 0.1 #100mA , 0.1A
+                if header == b'\xFF\xFE'  and data_size == 0x08:
+                    self.get_logger().info(f"[RX] Speed: {speed:.1f} RPM, Pos: {position:.1f}°, Current: {current:.1f} A")
+                else:
+                    self.get_logger().warn(f"[RX] Unexpected response")
+            else:
+                self.get_logger().warn("[RX] Ping: Incomplete response")   
+            time.sleep(0.01)  # CPU 낭비 방지용 짧은 대기
+
+    def speed_feedback_L(self):
+        while True:
+            response = self.ser_L.read(12)
             if response and len(response) >= 12:
                 header = response[0:2]
                 data_size = response[3]
@@ -95,22 +138,7 @@ class MotorControllerNode(Node):
             time.sleep(0.01)  # CPU 낭비 방지용 짧은 대기
 
 
-    def pos_feedback(self):
-        while True:
-            response = self.ser.read(12)
-            if response and len(response) >= 12:
-                header = response[0:2]
-                data_size = response[3]
-                position = ((response[7] << 8) | response[8]) * 0.01     # 스탭 단위 0.01 [degree]
-                speed = ((response[9] << 8) | response[10]) * 0.1 #0.1 [rpm]
-                current = response[11] * 0.1 #100mA , 0.1A
-                if header == b'\xFF\xFE'  and data_size == 0x08:
-                    self.get_logger().info(f"[RX] pos: {position:.1f}°, speed: {speed:.1f} RPM, Current: {current:.1f} A")
-                else:
-                    self.get_logger().warn(f"[RX] Unexpected Ping")
-            else:
-                self.get_logger().warn("[RX] Ping: Incomplete response")        
-            time.sleep(0.01)  # CPU 낭비 방지용 짧은 대기
+
 
 
 
